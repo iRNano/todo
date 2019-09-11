986b5d2538636d4e21a8faa40b3100a4c6bf705d
iNano <zub1online@yahoo.com>
2019-09-11 19:04:59 +0800

    2of100daysofcoding

--- script.js
+++ script.js
@@ -8,9 +8,38 @@ function inputLength(){
 	return input.value.length;
 }
 
+function createListItem(){
+	var li = document.createElement("li");
+	li.className = "list-group-item";
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+
+	//Append delete button
+
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	dBtn.className = "btn"
+	li.appendChild(dBtn);
+
+	dBtn.addEventListener('click',deleteListItem);
+
+	function deleteListItem(){
+		li.parentNode.removeChild(li);
+	}
+
+	li.addEventListener('click',done);
+
+	function done(){
+		li.classList.toggle("done");
+	}
+
+}
+
 function createListByAddButton(){
    if(input.value.length > 0){
    		console.log("createListByAddButton");
+   		createListItem();
    } else{
    	alert('Input text');
    }
@@ -19,6 +48,7 @@ function createListByAddButton(){
 function createListByKeypressed(keyPressed){
 	if(input.value.length > 0 && keyPressed.which === 13){
 		console.log("createListByKeypressed");
+		createListItem();
 	}else if(input.value.length === 0 && keyPressed.which === 13){
 		alert('Input text');
 	}
